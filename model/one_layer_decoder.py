d6191d1d4053b91081cabce78687380ed85cfbe0
WILLIAM BERGER <william.berger.7099@student.uu.se>
2024-01-08 23:14:08 +0100

    Removed comment

--- model/one_layer_decoder.py
+++ model/one_layer_decoder.py
@@ -1,8 +1,5 @@
 import torch.nn as nn
 
-# TODO:         # Feature map from Local-Net has size 1 x 1 x 128
-        # Feature map from ResNet-18 has size 1 x 1 x 512
-
 class OneLayerDecoder(nn.Module):
     """
     A simple one-layer decoder class that can be used to decode feature vectors
