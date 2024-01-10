f0a08ee6145c948c14c2088c65702c6635bd3a79
WILLIAM BERGER <william.berger.7099@student.uu.se>
2024-01-11 00:26:43 +0100

    Removed device handling from LocalNet

--- model/local_net.py
+++ model/local_net.py
@@ -5,9 +5,7 @@ class LocalNet(nn.Module):
         super().__init__()
 
         self.config = config
-        #self.device = torch.device(config['device']) # Set device as defined in yaml-file
 
-        # TODO: Should bias be true or false?
         # Image size is 33 x 33 x 3, outputs feature map size 1 x 1 x 128
         self.model = nn.Sequential(
             nn.Conv2d(3, 128, 3, stride=1, padding=0),
@@ -21,8 +19,6 @@ class LocalNet(nn.Module):
             nn.Conv2d(256, 128, 4, stride=1, padding=0),
             nn.LeakyReLU(negative_slope=self.config['leaky_relu_slope'])
         )
-
-        #self.model.to(self.device) # Move the model to the specified device
 
     def forward(self, x):
         x = self.model(x)
