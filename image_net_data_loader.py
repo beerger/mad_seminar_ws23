084363811315699050a415bfed76a9c57a958dfa
WILLIAM BERGER <william.berger.7099@student.uu.se>
2024-01-11 14:10:42 +0100

    Added option to cache images for ImageNetDataModule

--- image_net_data_loader.py
+++ image_net_data_loader.py
@@ -8,23 +8,39 @@ from torch.utils.data import Dataset, DataLoader
 from torchvision import transforms
 
 class ImageNetPatchesDataset(Dataset):
-    def __init__(self, image_paths, is_train=True):
+    def __init__(self, image_paths, is_train=True, cache_images=False):
         self.image_paths = image_paths
         self.is_train = is_train
         self.transform_local = self._build_transforms_local()
         self.transform_resnet = self._build_transforms_resnet()
+        self.cache = {} if cache_images else None # Initialize an empty cache if caching is enabled
 
     def __len__(self):
         return len(self.image_paths)
 
     def __getitem__(self, idx):
-        
+        if self.cache is not None:
+            # If caching is enabled and the image is cached, return it from the cache
+            if idx in self.cache:
+                patch_local, patch_resnet = self.cache[idx]
+            else:
+                # If the image is not cached, load and transform it, then add it to the cache
+                patch_local, patch_resnet = self.load_and_transform_image(idx)
+                self.cache[idx] = (patch_local, patch_resnet)
+        else:
+            # If caching is not enabled, simply load and transform the image without caching
+            patch_local, patch_resnet = self.load_and_transform_image(idx)
+
+        return patch_local, patch_resnet
+
+    def load_and_transform_image(self, idx):
         image_path = self.image_paths[idx]
         image = Image.open(image_path).convert('RGB')
 
         # Resize image to 256x256
         image = transforms.Resize((256, 256), interpolation=transforms.InterpolationMode.BILINEAR, antialias=True)(image)
 
+        # Create a patch for local and resnet transformations
         patch = transforms.RandomCrop(33)(image) if self.is_train else transforms.CenterCrop(33)(image)
 
         # Apply transformations
@@ -51,18 +67,41 @@ class ImageNetPatchesDataset(Dataset):
         ])
 
 class ImageNetDataModule(pl.LightningDataModule):
-    def __init__(self, train_image_paths, val_image_paths , batch_size=64, num_workers=4):
+    """
+    PyTorch Lightning data module for ImageNet dataset.
+
+    Attributes:
+        train_image_paths (list): List of paths to the training images.
+        val_image_paths (list): List of paths to the validation images.
+        batch_size (int): Batch size for training and validation data loaders.
+        num_workers (int): Number of workers to use for data loading.
+        cache_images (bool): If True, caches images in memory. Use with caution.
+    """
+    def __init__(self, train_image_paths, val_image_paths , batch_size=64, num_workers=4, cache_images=False):
         super().__init__()
         self.train_image_paths = train_image_paths
         self.val_image_paths = val_image_paths
         self.batch_size = batch_size
         self.num_workers = num_workers
+        self.cache_images = cache_images
+
+        if cache_images:
+            # Warning about memory usage
+            print("Warning: `cache_images` is set to True. Ensure you have enough memory to cache all images.")
 
     def train_dataloader(self):
-        train_dataset = ImageNetPatchesDataset(self.train_image_paths, is_train=True)
+        train_dataset = ImageNetPatchesDataset(
+            self.train_image_paths,
+            is_train=True, 
+            cache_images=self.cache_images
+        )
         return DataLoader(train_dataset, batch_size=self.batch_size, shuffle=True, num_workers=self.num_workers)
 
     def val_dataloader(self):
-        val_dataset = ImageNetPatchesDataset(self.val_image_paths, is_train=False)
+        val_dataset = ImageNetPatchesDataset(
+            self.val_image_paths, 
+            is_train=False, 
+            cache_images=self.cache_images
+        )
         return DataLoader(val_dataset, batch_size=self.batch_size, shuffle=False, num_workers=self.num_workers)
 
