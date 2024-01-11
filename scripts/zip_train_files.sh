a8ca30f5b4d2b3e392b7b31f7a117d7b5f1d4bdf
WILLIAM BERGER <william.berger.7099@student.uu.se>
2024-01-11 01:32:17 +0100

    Removed Jupyter Notebook-Specific Syntax, causing error

--- scripts/zip_train_files.sh
+++ scripts/zip_train_files.sh
@@ -1,5 +1,5 @@
 # Navigate to the dataset directory
-%cd /content/drive/MyDrive/AnomalyDetection/Datasets/ImageNet/train
+cd /content/drive/MyDrive/AnomalyDetection/Datasets/ImageNet/train
 
 # Create a zip file containing only directories (excluding .tar files)
-!find . -type d -name 'n*' -exec zip -r /content/drive/MyDrive/AnomalyDetection/Datasets/ImageNet/train.zip {} +
+find . -type d -name 'n*' -exec zip -r /content/drive/MyDrive/AnomalyDetection/Datasets/ImageNet/train.zip {} +
