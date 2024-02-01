653e04c32cc90be2b08376f3437eae48ab7e9132
WILLIAM BERGER <william.berger.7099@student.uu.se>
2024-02-01 01:54:54 +0100

    Added option do load specific pathology

--- data_loader/test_data_loader.py
+++ data_loader/test_data_loader.py
@@ -35,7 +35,7 @@ class TestDataset(Dataset):
 
         # Load positive mask
         pos_mask = Image.open(self.pos_mask_paths[idx]).convert('L')
-        pos_mask = pos_mask.resize(self.target_size, Image.NEAREST)
+        pos_mask = pos_mask.resize((256, 256), Image.NEAREST)
         pos_mask = transforms.ToTensor()(pos_mask)
 
         return img, pos_mask
@@ -47,44 +47,66 @@ class TestDataset(Dataset):
         ])
 
 
-def get_all_test_dataloader(split_dir: str, batch_size: int):
-    """
-    Loads all test data from split_dir, including all pathologies.
+class TestDataModule(pl.LightningDataModule):
 
-    @param split_dir: str
-        Path to directory containing the split files.
-    @param batch_size: int
-        Batch size.
-    """
-    pathologies = [
-        'absent_septum',
-        'artefacts',
-        'craniatomy',
-        'dural',
-        'ea_mass',
-        'edema',
-        'encephalomalacia',
-        'enlarged_ventricles',
-        'intraventricular',
-        'lesions',
-        'mass',
-        'posttreatment',
-        'resection',
-        'sinus',
-        'wml',
-        'other'
-    ]
-    
-    all_images = []
-    all_pos_masks = []
+    def __init__(self, split_dir: str, batch_size: int):
+        super().__init__()
+        self.split_dir = split_dir
+        self.batch_size = batch_size
 
-    for pathology in pathologies:
-        img_csv = os.path.join(split_dir, f'{pathology}.csv')
-        pos_mask_csv = os.path.join(split_dir, f'{pathology}_ann.csv')
+    def test_dataloader(self, pathology: str):
+        """
+        Loads test data from split_dir
 
-        all_images.extend(pd.read_csv(img_csv)['filename'].tolist())
-        all_pos_masks.extend(pd.read_csv(pos_mask_csv)['filename'].tolist())
+        @param pathology: str
+            pathology to load
+        """
+        img_csv = os.path.join(self.split_dir, f'{pathology}.csv')
+        pos_mask_csv = os.path.join(self.split_dir, f'{pathology}_ann.csv')
 
-    combined_dataset = TestDataset(all_images, all_pos_masks)
-    return DataLoader(combined_dataset, batch_size=batch_size, shuffle=False, drop_last=False)
+        return DataLoader(TestDataset(img_csv, pos_mask_csv),
+                          batch_size=self.batch_size,
+                          shuffle=False,
+                          drop_last=False)
+
+    def test_dataloader_all(self):
+        """
+        Loads all test data from split_dir, including all pathologies.
+
+        @param split_dir: str
+            Path to directory containing the split files.
+        @param batch_size: int
+            Batch size.
+        """
+        pathologies = [
+            'absent_septum',
+            'artefacts',
+            'craniatomy',
+            'dural',
+            'ea_mass',
+            'edema',
+            'encephalomalacia',
+            'enlarged_ventricles',
+            'intraventricular',
+            'lesions',
+            'mass',
+            'posttreatment',
+            'resection',
+            'sinus',
+            'wml',
+            'other'
+        ]
+
+        all_images = []
+        all_pos_masks = []
+
+        for pathology in pathologies:
+            img_csv = os.path.join(self.split_dir, f'{pathology}.csv')
+            pos_mask_csv = os.path.join(self.split_dir, f'{pathology}_ann.csv')
+
+            all_images.extend(pd.read_csv(img_csv)['filename'].tolist())
+            all_pos_masks.extend(pd.read_csv(pos_mask_csv)['filename'].tolist())
+
+        combined_dataset = TestDataset(all_images, all_pos_masks)
+        return DataLoader(combined_dataset, batch_size=self.batch_size, shuffle=False, drop_last=False)
 
